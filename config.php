8fcffef4b2e356ff2d9f6a46338700e6ad0f4b61
Qambar Raza <qambar@bozims.com>
2012-09-25 10:52:41 +0100

    config details removed

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(1);
+
 ini_set('display_errors','On');
 $config['atk']['base_path']='./atk4/';
 $config['dsn']='mysql://root:@localhost/fbwallmessage';
@@ -8,8 +8,8 @@ $config['url_postfix']='';
 $config['url_prefix']='?page=';
 
 # Facebook Wall Message Sender Application Configuration
-$config['appId']	='325002274262209';
-$config['secret']	='464fd58d412a67711a202a2324c790b9';
+$config['appId']	='xxxxxxxxxxxxxxx';
+$config['secret']	='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 
 
